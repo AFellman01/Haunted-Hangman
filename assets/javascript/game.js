c9515540a3ec606e10f498989ff8566296ba34e3
Alexander Fellman <alexanderfellman@Alexanders-MacBook-Pro.local>
2017-10-25 09:59:47 -0400

    Text

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+
+
+
+var words = ['storm', 'death', 'murder', 'crow', 'ghost', 'haunt', 'monster', 'aubergine', 'horror', 'scream', 'vampire', 'spider', 'fright', 'tree', 'halloween', 'pumpkin','bat','cemetary', 'coffin', 'scarecrow'];
--- assets/script/script.js
+++
@@ -1,4 +0,0 @@
-
-
-
-var words = ['storm', 'death', 'murder', 'crow', 'ghost', 'haunt', 'monster', 'aubergine', 'horror', 'scream', 'vampire', 'spider', 'fright', 'tree', 'halloween', 'pumpkin','bat','cemetary', 'coffin', 'scarecrow'];
